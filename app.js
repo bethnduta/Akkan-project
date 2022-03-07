28b500f4a8e9956c2f869df99b89abf62932ea57
bethnduta <85553801+bethnduta@users.noreply.github.com>
2022-03-07 15:54:32 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-var femaleNames = ["Akosue", "Adwoa", "Abenaa", "AKue", "Yaa", "Afua", "Ama"];
-
-var genders = document.getElementById("gender");
-var gender = genders.options[genders.selectedIndex].text;
-console.log(gender)
-
 function generateName() {
     var date = document.getElementById('date').value;
     var month = document.getElementById('month').value;
     var year = document.getElementById('year').value;
     var birthDay = new Date(year + '/' + month + '/' + date);
     var dayOfWeek = birthDay.getDay();
+
+    var genders = document.getElementById("gender");
+    var gender = genders.options[genders.selectedIndex].text;
+
+    var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var femaleName = ["Akosue", "Adwoa", "Abenaa", "AKue", "Yaa", "Afua", "Ama"];
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
     if (Number(date) <= 0 && Number(date) > 31) {
         document.getElementById("result").innerHTML="This is an invalid date!"
@@ -24,10 +23,10 @@ function generateName() {
 
     }
     else if (gender === "Male") {
-        document.getElementById("Output").innerHTML="You were born on +"+ days[daysOfWeek]+" and "+" Your Akan name is "+maleName[dayOfWeek]
+        document.getElementById("result").innerHTML="You were born on "+ days[dayOfWeek]+" and "+" Your Akan name is "+maleName[dayOfWeek]
     }
     else if (gender === "Female") {
-        document.getElementById("Output").innerHTML="You were born on +"+ days[daysOfWeek]+" and "+" Your Akan name is "+femaleName[dayOfWeek]
+        document.getElementById("result").innerHTML="You were born on "+ days[dayOfWeek]+" and "+" Your Akan name is "+femaleName[dayOfWeek]
     }
-    else{document.getElementById("Output").innerHTML=" Wrong input! "}
+    else{document.getElementById("result").innerHTML=" Wrong input! "}
 }
